22d62da0e38c1c332a6b01f9952b05b561335f54
Bailey McKelway <bjlmckelway@gmail.com>
2020-06-09 15:44:27 -0400

    done with saved list

--- client/components/SavedItems.js
+++ client/components/SavedItems.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Message from './Message';
 
-export default class SaveItems extends Component {
-  //component on mount makes axios call getting saved items
+class SaveItems extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: this.props.user,
+      messages: this.props.messages,
+    };
+  }
+  componentDidMount() {
+    const savedMessages = this.state.user.saved;
+    const messages = this.state.messages;
+    let newMessages = [];
+
+    messages.map((message) => {
+      if (savedMessages.indexOf(message.id) !== -1) {
+        newMessages.push(message);
+      }
+    });
+    this.setState({
+      messages: newMessages,
+    });
+  }
+
   render() {
-    return <h1>Saved Items</h1>;
-    // display saved items similar to messages, add a jump to comment button
+    return (
+      <div>
+        <ul className="media-list">
+          {this.state.messages.map((message) => (
+            <Message message={message} key={message.id} />
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+    messages: state.messages,
+  };
+};
+
+export default connect(mapStateToProps, null)(SaveItems);
